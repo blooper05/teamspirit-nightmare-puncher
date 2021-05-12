40d88a662dd9598ea2fd424f601739fc3d050160
blooper05 <legend.of.blooper@gmail.com>
2021-05-12 18:59:59 +0900

    :sparkles: (feat): Use anonymous async function for punch actions

--- src/punchOut.ts
+++ src/punchOut.ts
@@ -6,15 +6,17 @@ const url = 'https://teamspirit.ap0.visual.force.com/apex/AtkWorkComponent';
 const username = process.env.TEAMSPIRIT_USERNAME;
 const password = process.env.TEAMSPIRIT_PASSWORD;
 
-nightmare
-  .goto(url)
-  .wait(0)
-  .insert('#username', '')
-  .insert('#password', '')
-  .insert('#username', username)
-  .insert('#password', password)
-  .uncheck('#rememberUn')
-  .click('#Login')
-  .wait('#btnEtInput.pw_btnnet')
-  .click('#btnEtInput')
-  .end();
+(async () => {
+  await nightmare
+    .goto(url)
+    .wait(0)
+    .insert('#username', '')
+    .insert('#password', '')
+    .insert('#username', username)
+    .insert('#password', password)
+    .uncheck('#rememberUn')
+    .click('#Login')
+    .wait('#btnEtInput.pw_btnnet')
+    .click('#btnEtInput')
+    .end();
+})();
